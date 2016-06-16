26c3d7df6fce286d07e942db8d71c26e6d2d08e8
Landon Bass <landon@labtechnologygroup.com>
2016-06-16 00:29:39 +0000

    initial bayes theroem model

--- src/MLDotNet/HamOrSpam/Script.fsx
+++ src/MLDotNet/HamOrSpam/Script.fsx
@@ -1,1 +1,47 @@
 ﻿
+open System.IO
+
+type DocType =
+    | Ham
+    | Spam
+
+let parseDocType (label: string) =
+    match label with
+    | "ham"  -> Ham
+    | "spam" -> Spam
+    | _      -> failwith "unknown label"
+
+let parseLine (line: string) =
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\..\Data\Ch2\" + fileName
+
+let dataset =
+    File.ReadAllLines path
+    |> Array.map parseLine
+
+let spamWithFREE =
+    dataset
+    |> Array.filter (fun (docType, _) -> docType = Spam)
+    |> Array.filter (fun (_, sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let hamWithFREE =
+    dataset
+    |> Array.filter (fun (docType, _) -> docType = Ham)
+    |> Array.filter (fun (_, sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let primitiveClassifier (sms: string) =
+    if (sms.Contains "FREE")
+    then Spam
+    else Ham
+
+open NaiveBayes.Classifier
+Hello "landon"
+
+
