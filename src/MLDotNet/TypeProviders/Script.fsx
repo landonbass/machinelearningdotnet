3fc46ed4b95e923306473e5d81f93955dc26933d
Landon Bass <landon@labtechnologygroup.com>
2016-06-17 00:23:37 +0000

    ch3 section 1 completed - stack overflow json typeprovider

--- src/MLDotNet/TypeProviders/Script.fsx
+++ src/MLDotNet/TypeProviders/Script.fsx
@@ -1,8 +1,55 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿
+#I @"..\packages\"
+#r @"FSharp.Data.2.3.0\lib\net40\FSharp.Data.dll"
 
-#load "Library1.fs"
-open TypeProviders
+open FSharp.Data
 
-// Define your library scripting code here
+type Questions = JsonProvider<"""https://api.stackexchange.com/2.2/questions?site=stackoverflow""">
 
+let csQuestions = """https://api.stackexchange.com/2.2/questions/?site=stackoverflow&tagged=C%23"""
+
+Questions.Load(csQuestions).Items |> Seq.iter (fun q -> printfn "%s" q.Title)
+
+[<Literal>]
+let sample = """{"people":[
+    {"name":"landon bass", "age":33}
+    ]}"""
+type HardCodedPerson = JsonProvider<sample>
+
+let questionQuery = """https://api.stackexchange.com/2.2/questions?site=stackoverflow"""
+
+let tagged tags query =
+    let joinedTags = tags |> String.concat ";"
+    sprintf "%s&tagged=%s" query joinedTags
+
+let page p query = sprintf "%s&page=%i" query p
+
+let pageSize s query = sprintf "%s&pagesize=%i" query s
+
+let extractQuestions (query: string) = Questions.Load(query).Items
+
+let ``C#`` = "C%23"
+let ``F#`` = "F%23"
+
+let fsSample = 
+    questionQuery
+    |> tagged [``F#``]
+    |> pageSize 100
+    |> extractQuestions
+
+let csSample = 
+    questionQuery
+    |> tagged [``C#``]
+    |> pageSize 100
+    |> extractQuestions
+
+let analyzeTags (qs: Questions.Item seq) =
+    qs
+    |> Seq.collect (fun question -> question.Tags)
+    |> Seq.countBy id
+    |> Seq.filter (fun (_, count) -> count > 2)
+    |> Seq.sortBy (fun (_, count) -> -count)
+    |> Seq.iter (fun (tag, count) -> printfn "%s,%i" tag count)
+
+analyzeTags fsSample
+analyzeTags csSample
