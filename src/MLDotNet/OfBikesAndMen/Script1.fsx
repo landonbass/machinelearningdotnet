02066955d68cd1eaa85a1fa024b65b26338cc21b
Landon Bass <landon@labtechnologygroup.com>
2016-06-27 20:43:26 -0400

    mathnet linear algebra refresher

---
+++ src/MLDotNet/OfBikesAndMen/Script1.fsx
@@ -0,0 +1,46 @@
+﻿
+#I @"..\packages\"
+#r @"FSharp.Data.2.3.1\lib\net40\FSharp.Data.dll"
+#load @"FSharp.Charting.0.90.14\FSharp.Charting.fsx"
+#r @"MathNet.Numerics.3.11.1\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.3.11.1\lib\net40\MathNet.Numerics.FSharp.dll"
+
+open FSharp.Charting
+open FSharp.Data
+open MathNet
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+
+let A = vector[1.;2.;3.]
+let B = matrix [[1.;2.]
+                [3.;4.]
+                [5.;6.]]
+
+let C = A * A
+let D = A * B
+let E = A * B.Column(1)
+
+
+type Data = CsvProvider<"..\Data\Ch4\day.csv">
+let dataset = Data.Load("..\Data\Ch4\day.csv")
+let data = dataset.Rows
+
+type Vec = Vector<float>
+type Mat = Matrix<float>
+
+let cost (theta: Vec) (Y: Vec) (X: Mat) =
+    let ps = Y - (theta * X.Transpose())
+    ps * ps |> sqrt
+
+let predict (theta: Vec) (v: Vec) = theta * v
+
+let X = matrix [for obs in data -> [1.; float obs.Instant]]
+let Y = vector [for obs in data -> float obs.Cnt]
+
+let theta = vector [6000.; -4.5]
+cost theta Y X
+
+let estimate (Y:Vec) (X:Mat) =
+    (X.Transpose() * X).Inverse() * X.Transpose() * Y
+
+
