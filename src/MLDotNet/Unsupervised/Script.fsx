1cff05c621c4eb110564de177951b8200ac844ea
Landon Bass <landon@labtechnologygroup.com>
2016-08-16 07:35:20 -0400

    file cleanup

--- src/MLDotNet/Unsupervised/Script.fsx
+++ src/MLDotNet/Unsupervised/Script.fsx
@@ -2,12 +2,12 @@
 open System
 open System.IO
 
-let folder = @"C:\projects\machinelearningdotnet\src\MLDotNet\Unsupervised"
-let file = "userprofiles-toptags.txt"
+let file = @"C:\projects\machinelearningdotnet\src\MLDotNet\Unsupervised\userprofiles-toptags.txt"
+
 
 let headers, observations =
     let raw =
-        folder + "\\" + file
+        file
         |> File.ReadAllLines
 
     let headers = (raw.[0].Split ',').[1..]
@@ -27,3 +27,16 @@ headers
         let max = col |> Array.max
 
         printfn "%16s %8.1f %8.1f %8.1f" name avg min max)
+
+#r @"..\packages\FSharp.Charting.0.90.14\lib\net40\FSharp.Charting.dll"
+#load @"..\packages\FSharp.Charting.0.90.14\FSharp.Charting.fsx"
+open FSharp.Charting
+let labels = ChartTypes.LabelStyle(Interval=0.25)
+
+headers
+|> Seq.mapi (fun i name ->
+    name,
+    observations
+    |> Seq.averageBy (fun obs -> obs.[i]))
+|> Chart.Bar
+|> fun chart -> chart.WithXAxis(LabelStyle=labels)
