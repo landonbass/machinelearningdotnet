b0479fb64a29607bf6e4aa3d22d29403c2b8c3a9
Landon Bass <landon@labtechnologygroup.com>
2016-08-16 07:41:46 -0400

    kmeans clustering

---
+++ src/MLDotNet/Unsupervised/KMeans.fs
@@ -0,0 +1,58 @@
+﻿
+module KMeans
+
+    let pickFrom size k =
+        let rng = System.Random()
+        let rec pick (set:int Set) =
+            let candidate = rng.Next(size)
+            let set = set.Add candidate
+            if set.Count = k then set
+            else pick set
+        pick Set.empty |> Set.toArray
+
+    let initialize observations k =
+        let size = Array.length observations
+        let centroids =
+            pickFrom size k
+            |> Array.mapi (fun i index ->
+                i+ 1, observations.[index])
+
+        let assignments =
+            observations
+            |> Array.map (fun x -> 0, x)
+
+        (assignments, centroids)
+
+
+    let clusterize distance centroidOf observations k =
+        let rec search (assignments, centroids) =
+            let classifier observation =
+                centroids
+                |> Array.minBy (fun (_, centroid) ->
+                    distance observation centroid)
+                |> fst
+            let assignments' =
+                assignments
+                |> Array.map (fun (_, observation) ->
+                    let closestCentroidId = classifier observation
+                    (closestCentroidId, observation))
+            let changed =
+                (assignments, assignments')
+                ||> Seq.zip
+                |> Seq.exists (fun ((oldClusterId, _), (newClusterId, _)) ->
+                    not (oldClusterId = newClusterId))
+
+            if changed
+            then
+                let centroids' =
+                    assignments'
+                    |> Seq.groupBy fst
+                    |> Seq.map (fun (clusterID, group) ->
+                        clusterID, group |> Seq.map snd |> centroidOf)
+                    |> Seq.toArray
+                search (assignments',centroids')
+            else centroids,classifier
+
+        let initialValues = initialize observations k
+        search initialValues
+
--- src/MLDotNet/Unsupervised/Script.fsx
+++ src/MLDotNet/Unsupervised/Script.fsx
@@ -40,3 +40,47 @@ headers
     |> Seq.averageBy (fun obs -> obs.[i]))
 |> Chart.Bar
 |> fun chart -> chart.WithXAxis(LabelStyle=labels)
+
+#load "KMeans.fs"
+open KMeans
+
+type Observation = float []
+let features = headers.Length
+let distance (obs1: Observation) (obs2: Observation) =
+    (obs1, obs2)
+    ||> Seq.map2 (fun u1 u2 -> pown (u1 - u2) 2)
+    |> Seq.sum
+
+let centroidOf (cluster:Observation seq) =
+    Array.init features (fun f ->
+        cluster
+        |> Seq.averageBy (fun user -> user.[f]))
+
+let observations1 =
+    observations
+    |> Array.map (Array.map float)
+    |> Array.filter (fun x -> Array.sum x > 0.)
+
+let (clusters1, classifier1) =
+    let clustering = clusterize distance centroidOf
+    let k = 5
+    clustering observations1 k
+
+clusters1
+|> Seq.iter (fun (id, profile) ->
+    printfn "CLUSTER %i" id
+    profile
+    |> Array.iteri (fun i value -> printfn "%16s %.1f" headers.[i] value))
+
+Chart.Combine [
+    for (id, profile) in clusters1 ->
+        profile
+        |> Seq.mapi (fun i value -> headers.[i], value)
+        |> Chart.Bar
+    ]
+|> fun chart -> chart.WithXAxis(LabelStyle=labels)
+
+observations1
+|> Seq.countBy (fun obs -> classifier1 obs)
+|> Seq.iter (fun (clusterId, count) ->
+    printfn "cluster %i: %i elements" clusterId count)
